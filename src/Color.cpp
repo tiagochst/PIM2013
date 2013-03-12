35331fced0e321fb48116e9b1cbb4d4b83e412df
Vinicius Gardelli <gardelli123@gmail.com>
2013-03-12 16:48:32 +0100

    Fixed copy constructor and assignment operator for class Color.
    Signed-off-by: Vinicius Gardelli <gardelli123@gmail.com>

--- src/Color.cpp
+++ src/Color.cpp
@@ -1,25 +1,43 @@
 #include <Color.h>
+#define max(x, y) ((x > y) ? x : y)
+#define min(x, y) ((x < y) ? x : y)
 
-
-float fclamp(const float min, const float max, const float val) {
-    return max(min, min(max, val));
+float fclamp(const float iMin, const float iMax, const float iVal) {
+    return max(iMin, min(iMax, iVal));
 }
 
-Color::Color(int red, int green, int blue, int alpha) {
-    m_R = (red) / (255.0f);
-    m_G = (green) / (255.0f);
-    m_B = (blue) / (255.0f);
-    m_A = (alpha) / (255.0f);
+Color::Color(const Color& iSource) {
+    if ( this != &(iOther) ) {
+        m_R = iOther.m_R; 
+        m_G = iOther.m_G; 
+        m_B = iOther.m_B; 
+        m_A = iOther.m_A;
+    }
+}
+Color& operator=(const Color& iOther) {
+    if ( this != &(iOther) ) {
+        m_R = iOther.m_R; 
+        m_G = iOther.m_G; 
+        m_B = iOther.m_B; 
+        m_A = iOther.m_A;
+    }
+}
+
+Color::Color(int iRed, int iGreen, int iBlue, int iAlpha) {
+    m_R = (iRed) / (255.0f);
+    m_G = (iGreen) / (255.0f);
+    m_B = (iBlue) / (255.0f);
+    m_A = (iAlpha) / (255.0f);
 
     ClampValues();
 }
 
-Color::Color(const float red, const float green, const float blue, 
-                const float alpha) {
-    m_R = red;
-    m_G = green;
-    m_B = blue;
-    m_A = alpha;
+Color::Color(const float iRed, const float iGreen, const float iBlue, 
+                const float iAlpha) {
+    m_R = iRed;
+    m_G = iGreen;
+    m_B = iBlue;
+    m_A = iAlpha;
 
     ClampValues();
 }

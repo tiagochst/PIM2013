a62b1363663394a28d92ed937126879416c8f914
Vinicius Dias de Oliveira Gardelli <gardelli123@gmail.com>
2013-04-05 09:33:40 +0200

    Removed output files from version control. Moved KinectConfig to Config directory. Moved input files to Data directory. Created Config class in main.cpp to deal with settings files. Improved Image::PatternSearch code, but it still isn't working.

--- src/Image.cpp
+++ src/Image.cpp
@@ -11,16 +11,21 @@ inline bool InRange(
     return ( ( iVal >= iMin  ) && ( iVal <= iMax ) );
 }
 
+inline int abs(int iVal) {
+    return (iVal < 0) ? -iVal : iVal;
+}
+
+inline int max(int a, int b) {
+    return ( (a >= b) ? a : b );    
+}
+
 Image::Image(const int iWidth, const int iHeight, const int iGreyLevel)
     : m_height( iHeight ), 
       m_width( iWidth ), 
       m_maxGreyLevel( iGreyLevel )
 {
     m_figure.resize( m_height, m_width );
-}
-
-inline int abs(int iVal) {
-    return (iVal < 0) ? -iVal : iVal;
+    m_normalisedFigure.resize( m_height, m_width );
 }
 
 Image::Image( const std::string& iFilename )
@@ -35,11 +40,8 @@ void Image::LoadFromFile( const std::string& iFilename )
     std::stringstream ss;
     std::string inputLine = "";
 
-    /* path to our images */
-    std::string path = "./resources/Project/Images/";
-
     /* Opening pgm file*/
-    std::ifstream inFile( ( path + iFilename ).c_str(), 
+    std::ifstream inFile( iFilename.c_str(), 
                             std::ifstream::in | std::ifstream::binary );
 
     /* First line : version of pgm file*/
@@ -63,8 +65,8 @@ void Image::LoadFromFile( const std::string& iFilename )
     getline( inFile, inputLine );
     std::cout << "Comment : " << inputLine << std::endl;
 
-    /* Third line : size
-       Forth line : grey level*/
+    /* Third  line : size
+       Fourth line : grey level*/
     inFile >> height >> width >> greyLevel ;
 
     SetHeight( height );
@@ -73,11 +75,13 @@ void Image::LoadFromFile( const std::string& iFilename )
 
     /* resize matrix to receive the image */
     m_figure.resize( m_height, m_width );
+    m_normalisedFigure.resize( m_height, m_width );
 
     if ( isBinary ) {
         for ( i = 0; i < height; i++ )
             for ( j = 0; j < width; j++ ) {
                 m_figure( i, j ) = static_cast<int>( inFile.get() );
+                m_normalisedFigure( i , j ) = (float)m_figure( i, j ) / (float)(greyLevel);
             }
 
     } else {
@@ -86,6 +90,7 @@ void Image::LoadFromFile( const std::string& iFilename )
         for ( i = 0; i < height; i++ )
             for ( j = 0; j < width; j++ ) {
                 ss >> m_figure( i, j );
+                m_normalisedFigure( i , j ) = (float)m_figure( i, j ) / (float)(greyLevel);
             }
     }
 
@@ -100,8 +105,7 @@ Image::~Image()
 void Image::CreateAsciiPgm( const std::string& iFilename )
 {
     int i = 0, j = 0;
-    std::string prefix = "PROG";
-    std::ofstream ostr( ( prefix + iFilename ).c_str() );
+    std::ofstream ostr( iFilename.c_str() );
 
     /* writing header */
     ostr << "P2 " << std::endl;
@@ -117,75 +121,104 @@ void Image::CreateAsciiPgm( const std::string& iFilename )
     ostr.close();
 }
 
-int& Image::operator()( const int iRow, const int iCol )
+float& Image::operator()( const int iRow, const int iCol )
 {
-    if ( InRange( iCol, 0, m_width - 1) &&
-         InRange( iRow, 0, m_height - 1) ) {
+    if ( InRange( iCol, 0, m_width  - 1 ) &&
+         InRange( iRow, 0, m_height - 1 ) ) {
 
-        return m_figure(iRow, iCol);
+        return m_normalisedFigure(iRow, iCol);
 
     }
     throw BadIndex();
 }
-int Image::operator()( const int iRow, const int iCol ) const
+float Image::operator()( const int iRow, const int iCol ) const
 {
-    if ( InRange( iCol, 0, m_width - 1) &&
-         InRange( iRow, 0, m_height - 1) ) {
+    if ( InRange( iCol, 0, m_width  - 1 ) &&
+         InRange( iRow, 0, m_height - 1 ) ) {
 
-        return m_figure(iRow, iCol);
+        return m_normalisedFigure(iRow, iCol);
 
     } else {
 
-        return 0;
+        return 0.0f;
 
     }
 }
 
-Image Image::Correlation( const Image& iOther ) const
+int Image::Correlation( const Image& iOther ) const
+{
+}
+
+Image Image::PatternSearch( const Image& iMask, Coordinate& oBestMatch ) const
 {
     const Image& me = (*this);
-    Image correlation(m_width, m_height, m_maxGreyLevel);
+    Image correlation( m_width - iMask.GetWidth() + 1, 
+                        m_height - iMask.GetHeight() + 1, 
+                        65535);
 
-    for ( int i = 0; i < m_height; i++ ) {
-        for ( int j = 0; j < m_width; j++ ) {
-            int val = 0;
-            int myMean = 0;
-            int otherMean = 0;
-            
-            for ( int k = -4; k < 5; k++ ) {
-                for ( int l = -4; l < 5; l++ ) {
-                    myMean    += me(i + k, j + l); 
-                    otherMean += iOther(i + k, j + l);
-                }
-            }
-            myMean /= 81;
-            otherMean /= 81;
-
-            for ( int k = -4; k < 5; k++ ) {
-                for ( int l = -4; l < 5; l++ ) {
-                    val += (me(i + k, j + l) - myMean) * (iOther(i + k, j + l) - otherMean);
-                }
-            }
-            
-            correlation(i, j) = sqrt(val) / 81;
+    int bestMatchVal = 0;
+    int minVal = 0;
+    int nMaskElems = iMask.m_width * iMask.m_width;
+    
+    float maskDenom = 0;
+    for ( int i = 0; i < iMask.m_height; i++ ) {
+        for ( int j = 0; j < iMask.m_width; j++ ) {
+            maskDenom += iMask( i, j ) * iMask( i, j );
         }
     }
-    
+
+    for ( int i = 0; i < correlation.GetHeight(); i++ ) {
+        for ( int j = 0; j < correlation.GetWidth(); j++ ) {
+
+            float val = 0;
+            float myDenom = 0;
+            for ( int k = 0; k < iMask.GetHeight(); k++ ) {
+                for ( int l = 0; l < iMask.GetWidth(); l++ ) {
+                    float myVal   = me( i + k, j + l );
+                    float maskVal = iMask( k, l );
+
+                    myDenom += myVal * myVal; 
+                    
+                    val += myVal * maskVal;
+                }
+            }
+            val /= sqrt(maskDenom * myDenom);
+            std::cout << val << " ";
+            
+            correlation(i, j) = val;
+            if ( val > bestMatchVal ) {
+                bestMatchVal = val;
+                oBestMatch.x = i;
+                oBestMatch.y = j; 
+            }
+        }
+    }
+    correlation.Recalculate();    
+
     return correlation;
 }
 
 Image Image::Difference( const Image& iOther ) const
 {
     const Image& me = (*this);
-    Image difference(m_width, m_height, m_maxGreyLevel);
+    Image difference( m_width, m_height, m_maxGreyLevel );
 
     for ( int i = 0; i < m_height; i++ ) {
         for ( int j = 0; j < m_width; j++ ) {
-            difference(i, j) = abs(me(i, j) - iOther(i, j));
+            difference( i, j ) = abs( me( i, j ) - iOther( i, j ) );
         }
     }
     
     return difference;
+}
+
+void Image::Recalculate()
+{
+    for ( int i = 0; i < m_height; i++ ) {
+        for ( int j = 0; j < m_width; j++ ) {
+            m_figure( i, j ) = m_normalisedFigure( i, j ) * m_maxGreyLevel;
+        }
+    }
 }
 
 void Image::SetHeight( const int iHeight )
@@ -203,17 +236,17 @@ void Image::SetGreyLevel( const int iGreyLevel )
     m_maxGreyLevel = iGreyLevel;
 }
 
-int Image::GetHeight()
+int const& Image::GetHeight() const
 {
     return m_height;
 }
 
-int Image::GetWidth()
+int const& Image::GetWidth() const
 {
     return m_width;
 }
 
-int Image::GetMaxGreyLevel()
+int const& Image::GetMaxGreyLevel() const
 {
     return m_maxGreyLevel;
 }

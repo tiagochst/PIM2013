18a081dfefb13ffb198f51da4947ed94e9a26714
Vinicius Dias de Oliveira Gardelli <gardelli123@gmail.com>
2013-05-24 14:20:45 +0200

    Added methods to save point clouds as .ply files on disk.
    Signed-off-by: Vinicius Dias de Oliveira Gardelli <gardelli123@gmail.com>

--- include/PointSet.h
+++ include/PointSet.h
@@ -10,13 +10,14 @@ private:
     std::vector<Vertex>     m_vertices;
     
 public:
-    PointSet(void);
-    PointSet(const std::string& iFilename);
-    ~PointSet(void);
+    PointSet ( void );
+    PointSet ( const std::string& iFilename );
+    ~PointSet ( void );
 
-    void LoadPlyFile(const char* iFilename);
-    void LoadFromFile(const std::string& iFilename);
-    void WriteToFile(const std::string& iFilename);
+    void LoadPlyFile ( const char* iFilename );
+    void LoadFromFile ( const std::string& iFilename );
+    void WritePlyFile ( const char* iFilename );
+    void WriteToFile ( const std::string& iFilename );
 
     void PushVertex ( const Vertex& iVertex );
 };

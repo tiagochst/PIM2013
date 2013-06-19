4dc40e12b7a1a1277412d369a38950e37b0ca37a
Vinicius Dias de Oliveira Gardelli <gardelli123@gmail.com>
2013-06-19 12:47:25 +0200

    Added faces and UV coordinates to point cloud load/save. Also added counters for number of vertices and faces.
    Signed-off-by: Vinicius Dias de Oliveira Gardelli <gardelli123@gmail.com>

--- include/PointSet.h
+++ include/PointSet.h
@@ -5,9 +5,16 @@
 #include <vector>
 #include "Vertex.h"
 
+struct Face {
+    unsigned int v0, v1, v2;
+};
+
 class PointSet {
 private:
+    unsigned int            m_vtxCount;
+    unsigned int            m_faceCount;
     std::vector<Vertex>     m_vertices;
+    std::vector<Face>       m_faces;
     
 public:
     PointSet ( void );
@@ -20,16 +27,48 @@ public:
     void WriteToFile ( const std::string& iFilename );
 
     void PushVertex ( const Vertex& iVertex );
+    void PushFace ( const Face& iFace );
 
     void MoveToBarycenter ();
 
     void Draw () const;
+
+    const Vertex& GetVertex ( const unsigned int& iVertex ) const;
+    const Face& GetFace ( const unsigned int& iFace ) const;
+
+    const unsigned int& GetNumVertices () const;
+    const unsigned int& GetNumFaces () const;
 };
 
 inline void PointSet::PushVertex (
     const Vertex& iVertex
 ) {
+    m_vtxCount++;
     m_vertices.push_back ( iVertex );
 }
 
+inline void PointSet::PushFace (
+    const Face& iFace
+) {
+    m_faceCount++;
+    m_faces.push_back ( iFace );
+}
+
+inline const Vertex& PointSet::GetVertex ( const unsigned int& iVertex ) const {
+    return m_vertices[iVertex];
+}
+inline const Face& PointSet::GetFace ( const unsigned int& iFace ) const {
+    return m_faces[iFace];
+}
+
+inline const unsigned int& PointSet::GetNumVertices ()
+const {
+    return m_vtxCount;
+}
+
+inline const unsigned int& PointSet::GetNumFaces ()
+const {
+    return m_faceCount;
+}
+ 
 #endif
--- src/PointSet.cpp
+++ src/PointSet.cpp
@@ -7,12 +7,13 @@
 typedef struct VertexPOD {
     float           x, y, z; // Position
     float           nx, ny, nz; // Normal
+    unsigned int    u, v; //UV coordinates
     unsigned char   red, green, blue, alpha; // Color
 } VertxPOD;
 
 typedef struct FacePOD {
     unsigned char   nVerts;    /* number of vertex indices in list */
-    int*            verts;     /* vertex index list */
+    unsigned int*   verts;     /* vertex index list */
 } FacePOD;
 
 static PlyProperty vertexProperties[] = {
@@ -22,17 +23,23 @@ static PlyProperty vertexProperties[] = {
     {            "nx", PLY_FLOAT, PLY_FLOAT, offsetof(VertexPOD,    nx), 0,         0,         0,                         0},
     {            "ny", PLY_FLOAT, PLY_FLOAT, offsetof(VertexPOD,    ny), 0,         0,         0,                         0},
     {            "nz", PLY_FLOAT, PLY_FLOAT, offsetof(VertexPOD,    nz), 0,         0,         0,                         0},
+    {             "u",  PLY_UINT,  PLY_UINT, offsetof(VertexPOD,     u), 0,         0,         0,                         0},
+    {             "v",  PLY_UINT,  PLY_UINT, offsetof(VertexPOD,     v), 0,         0,         0,                         0},
     {           "red", PLY_UCHAR, PLY_UCHAR, offsetof(VertexPOD,   red), 0,         0,         0,                         0},
     {         "green", PLY_UCHAR, PLY_UCHAR, offsetof(VertexPOD, green), 0,         0,         0,                         0},
     {          "blue", PLY_UCHAR, PLY_UCHAR, offsetof(VertexPOD,  blue), 0,         0,         0,                         0},
     {         "alpha", PLY_UCHAR, PLY_UCHAR, offsetof(VertexPOD, alpha), 0,         0,         0,                         0},
 };
 static PlyProperty faceProperties[] = {
-    {"vertex_indices",   PLY_INT,   PLY_INT, offsetof(  FacePOD, verts), 1, PLY_UCHAR, PLY_UCHAR, offsetof(FacePOD, nVerts)},
+    {"vertex_indices",  PLY_UINT,  PLY_UINT, offsetof(  FacePOD, verts), 1, PLY_UCHAR, PLY_UCHAR, offsetof(FacePOD, nVerts)},
 };
 
-PointSet::PointSet(void) {}
-PointSet::PointSet(const std::string& iFilename) {
+PointSet::PointSet(void)
+    :   m_vtxCount ( 0u ), m_faceCount ( 0u )
+{}
+PointSet::PointSet(const std::string& iFilename)
+    :   m_vtxCount ( 0u ), m_faceCount ( 0u )
+{
     LoadFromFile(iFilename);
 }
 PointSet::~PointSet(void) {}
@@ -50,26 +57,32 @@ void PointSet::WritePlyFile(const char* iFilename) {
     char*           filename = new char[pathLen + 1];
     memcpy(filename, iFilename, (pathLen + 1) * sizeof(char));
 
-    char* elem_names[] = { "vertex" };
+    char* elem_names[] = { "vertex", "face" };
 
     /* open a PLY file for reading */
-    plyFile = ply_open_for_writing(filename, 1, elem_names, PLY_ASCII, &version);
+    plyFile = ply_open_for_writing(filename, 2, elem_names, PLY_ASCII, &version);
     delete[] filename;
     filename = 0;
 
     /* describe what properties go into the vertex and face elements */
     ply_element_count ( plyFile, "vertex", m_vertices.size () );
 
-    ply_describe_property ( plyFile, "vertex", &vertexProperties[0] );
-    ply_describe_property ( plyFile, "vertex", &vertexProperties[1] );
-    ply_describe_property ( plyFile, "vertex", &vertexProperties[2] );
-    ply_describe_property ( plyFile, "vertex", &vertexProperties[3] );
-    ply_describe_property ( plyFile, "vertex", &vertexProperties[4] );
-    ply_describe_property ( plyFile, "vertex", &vertexProperties[5] );
-    ply_describe_property ( plyFile, "vertex", &vertexProperties[6] );
-    ply_describe_property ( plyFile, "vertex", &vertexProperties[7] );
-    ply_describe_property ( plyFile, "vertex", &vertexProperties[8] );
-    ply_describe_property ( plyFile, "vertex", &vertexProperties[9] );
+    ply_describe_property ( plyFile, "vertex", &vertexProperties[ 0] );
+    ply_describe_property ( plyFile, "vertex", &vertexProperties[ 1] );
+    ply_describe_property ( plyFile, "vertex", &vertexProperties[ 2] );
+    ply_describe_property ( plyFile, "vertex", &vertexProperties[ 3] );
+    ply_describe_property ( plyFile, "vertex", &vertexProperties[ 4] );
+    ply_describe_property ( plyFile, "vertex", &vertexProperties[ 5] );
+    ply_describe_property ( plyFile, "vertex", &vertexProperties[ 6] );
+    ply_describe_property ( plyFile, "vertex", &vertexProperties[ 7] );
+    ply_describe_property ( plyFile, "vertex", &vertexProperties[ 8] );
+    ply_describe_property ( plyFile, "vertex", &vertexProperties[ 9] );
+    ply_describe_property ( plyFile, "vertex", &vertexProperties[10] );
+    ply_describe_property ( plyFile, "vertex", &vertexProperties[11] );
+
+    ply_element_count ( plyFile, "face", m_faces.size () );
+
+    ply_describe_property ( plyFile, "face", &faceProperties[0] );
 
     ply_put_comment (plyFile, "author: PIM2013");
 
@@ -80,11 +93,15 @@ void PointSet::WritePlyFile(const char* iFilename) {
     /* set up and write the vertex elements */
     ply_put_element_setup ( plyFile, "vertex" );
     for (i = 0; i < m_vertices.size (); i++) {
-        Vertex v = m_vertices[i];
+        Vertex vtx = m_vertices[i];
 
-        const Vec3Df&   position    =   v.GetPosition ();
-        const Vec3Df&   normal      =   v.GetNormal ();
-        const Color&    color       =   v.GetColor ();
+        const Vec3Df&   position    =   vtx.GetPosition ();
+        const Vec3Df&   normal      =   vtx.GetNormal ();
+        const Color&    color       =   vtx.GetColor ();
+        unsigned int    u           =   0;
+        unsigned int    v           =   0;
+
+        vtx.GetUVCoord ( u, v );
 
         VertexPOD vPOD;
         vPOD.x      = position[0];
@@ -93,12 +110,31 @@ void PointSet::WritePlyFile(const char* iFilename) {
         vPOD.nx     = normal[0];
         vPOD.ny     = normal[1];
         vPOD.nz     = normal[2];
+        vPOD.u      = u; 
+        vPOD.v      = v; 
         vPOD.red    = 255.f * color.Red ();
         vPOD.green  = 255.f * color.Green ();
         vPOD.blue   = 255.f * color.Blue ();
         vPOD.alpha  = 255.f * color.Alpha ();
 
         ply_put_element ( plyFile, (void *) &vPOD );
+    }
+
+    ply_put_element_setup ( plyFile, "face" );
+    for (i = 0; i < m_faces.size (); i++) {
+        Face face = m_faces[i];
+
+        FacePOD fPOD;
+        fPOD.nVerts = 3;
+        fPOD.verts = new unsigned int[3];
+        fPOD.verts[0] = face.v0;
+        fPOD.verts[1] = face.v1;
+        fPOD.verts[2] = face.v2;
+
+        ply_put_element ( plyFile, (void *) &fPOD );
+
+        delete[] fPOD.verts;
+        fPOD.verts = (unsigned int*)0x0;
     }
 
     /* close the PLY file */
@@ -136,7 +172,6 @@ void PointSet::LoadPlyFile(const char* iFilename) {
     /* go through each kind of element that we learned is in the file */
     /* and read them */
     for ( int elem = 0; elem < nGlobalElems; elem++ ) {
-    //for ( int elem = nGlobalElems - 1; elem >= 0; elem-- ) {
         /* get the description of the first element */
         std::string elementName(elementNameList[elem]);
         char* cElementName = elementNameList[elem];
@@ -155,16 +190,18 @@ void PointSet::LoadPlyFile(const char* iFilename) {
         /* if we're on vertex elements, read them in */
         if ( elementName.compare("vertex") == 0 ) {
             /* set up for getting vertex elements */
-            ply_get_property(plyFile, cElementName, &vertexProperties[0]); // x
-            ply_get_property(plyFile, cElementName, &vertexProperties[1]); // y
-            ply_get_property(plyFile, cElementName, &vertexProperties[2]); // z
-            ply_get_property(plyFile, cElementName, &vertexProperties[3]); // nx
-            ply_get_property(plyFile, cElementName, &vertexProperties[4]); // ny
-            ply_get_property(plyFile, cElementName, &vertexProperties[5]); // nz
-            ply_get_property(plyFile, cElementName, &vertexProperties[6]); // red
-            ply_get_property(plyFile, cElementName, &vertexProperties[7]); // green
-            ply_get_property(plyFile, cElementName, &vertexProperties[8]); // blue
-            ply_get_property(plyFile, cElementName, &vertexProperties[9]); // alpha
+            ply_get_property(plyFile, cElementName, &vertexProperties[ 0]); // x
+            ply_get_property(plyFile, cElementName, &vertexProperties[ 1]); // y
+            ply_get_property(plyFile, cElementName, &vertexProperties[ 2]); // z
+            ply_get_property(plyFile, cElementName, &vertexProperties[ 3]); // nx
+            ply_get_property(plyFile, cElementName, &vertexProperties[ 4]); // ny
+            ply_get_property(plyFile, cElementName, &vertexProperties[ 5]); // nz
+            ply_get_property(plyFile, cElementName, &vertexProperties[ 6]); // u
+            ply_get_property(plyFile, cElementName, &vertexProperties[ 7]); // v
+            ply_get_property(plyFile, cElementName, &vertexProperties[ 8]); // red
+            ply_get_property(plyFile, cElementName, &vertexProperties[ 9]); // green
+            ply_get_property(plyFile, cElementName, &vertexProperties[10]); // blue
+            ply_get_property(plyFile, cElementName, &vertexProperties[11]); // alpha
                                                  
             /* grab all the vertex elements */   
             for ( int vertex = 0; vertex < nElems; vertex++ ) {
@@ -175,9 +212,10 @@ void PointSet::LoadPlyFile(const char* iFilename) {
                 Vec3Df p(vDesc.x , vDesc.y , vDesc.z );
                 Vec3Df n(vDesc.nx, vDesc.ny, vDesc.nz);
                 Color  c(vDesc.red, vDesc.green, vDesc.blue, vDesc.alpha);
-                Vertex v(p, n, c);
+                Vertex v(p, n, c, vDesc.u, vDesc.v);
                 
                 m_vertices.push_back(v);
+                m_vtxCount++;
                 
 #ifdef __DEBUG_PLY_READ
                 /* print out vertex x,y,z for debugging */
@@ -192,8 +230,10 @@ void PointSet::LoadPlyFile(const char* iFilename) {
                                                 << (int)vDesc.green << " "
                                                 << (int)vDesc.blue  << " " 
                                                 << (int)vDesc.alpha << std::endl;
+                std::cout << " - UVCoords = "   << vDesc.u << ", "
+                                                << vDesc.v << std::endl;
 #endif
-                }
+            }
         }
 
         /* if we're on face elements, read them in */
@@ -203,9 +243,17 @@ void PointSet::LoadPlyFile(const char* iFilename) {
 
             /* grab all the face elements */
             for ( int face = 0; face < nElems; face++ ) {
-                /* grab and element from the file */
+                /* grab an element from the file */
                 FacePOD f;
                 ply_get_element(plyFile, (void*)&f);
+
+                Face faceObj;
+                faceObj.v0 = f.verts[0];
+                faceObj.v1 = f.verts[1];
+                faceObj.v2 = f.verts[2];
+
+                m_faces.push_back ( faceObj );
+                m_faceCount++;
 
 #ifdef __DEBUG_PLY_READ
                 /* print out face info, for debugging */
@@ -275,7 +323,7 @@ void PointSet::Draw () const {
         const Vertex& v = m_vertices[vtx];
 
         const Color& c = v.GetColor ();
-        const Vec3Df& pos = v.GetPosition();
+        const Vec3Df pos = v.GetPosition()/5;
 
         glColor4f ( c.Red(), c.Green(), c.Blue(), c.Alpha() );
         glVertex3f ( pos[0], pos[1], pos[2] );
